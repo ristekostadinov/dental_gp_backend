c71334e2efd1ecd6e811474f784d38e889482c27
vtodev <viktortodev@gmail.com>
2025-01-06 16:03:16 +0100

    [no-issue] cosmetics

--- src/main/java/com/example/demo/services/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/services/impl/UserServiceImpl.java
@@ -10,7 +10,6 @@ import com.example.demo.services.UserService;
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.NotImplementedException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -47,7 +46,7 @@ public class UserServiceImpl implements UserService {
 
     public List<UserView> findAll() {
 //        return this.userRepository.listUsers();
-        throw new NotImplementedException("Not implemented yet");
+        throw new RuntimeException("Not implemented yet");
     }
 
     @Transactional
