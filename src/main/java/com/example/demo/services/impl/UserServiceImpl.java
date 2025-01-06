2aae4ac2111f8064d2d4ffce92877bd4baa301ab
vtodev <viktortodev@gmail.com>
2025-01-06 15:10:41 +0100

    [no-issue] fix build: - update spring boot version to 3.4.1 - disable DEBUG mod on logging - comment out wrong code

--- src/main/java/com/example/demo/services/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/services/impl/UserServiceImpl.java
@@ -10,6 +10,7 @@ import com.example.demo.services.UserService;
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.NotImplementedException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -45,7 +46,8 @@ public class UserServiceImpl implements UserService {
     }
 
     public List<UserView> findAll() {
-        return this.userRepository.listUsers();
+//        return this.userRepository.listUsers();
+        throw new NotImplementedException("Not implemented yet");
     }
 
     @Transactional
